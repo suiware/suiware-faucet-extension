c5e88640fba5a6a7d08920d2f11a1698c4fcfc2a
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-12-17 14:57:05 +0100

    Working on the popup.

--- popup.js
+++ popup.js
@@ -1,27 +1,40 @@
-const address = document.getElementById("address");
-const faucet = document.getElementById("faucet");
+const faucetForm = document.querySelector(".faucet-form");
+const fundButton = faucetForm.querySelector(".fund-button");
 
-faucet.addEventListener("click", async () => {
-  const address = document.getElementById("address").value;
-  if (address.length !== 42) {
+const validateAddress = (address) => {
+  return address.match(/0[xX][a-fA-F0-9]{64}/);
+};
+
+fundButton.addEventListener("click", async () => {
+  const selectedNetwork = faucetForm.querySelector(
+    'input[name="network"]:checked'
+  ).value;
+  const address = faucetForm.querySelector(".address").value;
+
+  if (validateAddress(address) === null) {
     alert("Invalid address");
     return;
   }
-  const response = await fetch("https://faucet.devnet.sui.io/gas", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      FixedAmountRequest: {
-        recipient: address,
-        amount: 1000,
+
+  const response = await fetch(
+    `https://faucet.${selectedNetwork}.sui.io/v1/gas`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-    }),
-  });
+      body: JSON.stringify({
+        FixedAmountRequest: {
+          recipient: address,
+        },
+      }),
+    }
+  );
+
   if (response.ok) {
-    alert("Faucet sent");
-  } else {
-    alert("Faucet failed");
+    alert("Funded successfully");
+    return;
   }
+
+  alert(`Failed to fund: ${response.statusText}`);
 });
